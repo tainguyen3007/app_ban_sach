2fb2a379ae6464f7727af2195b6e7b913750f5cf
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-06-11 12:58:43 +0700

    update main.dart by Tai

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World vuduchoang'),
+          child: Text('Hello World nhánh main'),
         ),
       ),
     );
