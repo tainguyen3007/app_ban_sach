2ede6a301267485ad5cb951ff115678aec987ff1
mphuc0902 <minhphucdang952@gmail.com>
2025-06-12 10:44:57 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello Phuc!'),
+          child: Text('Hello Phuc88888'),
         ),
       ),
     );
