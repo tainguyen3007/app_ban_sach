1b324420fcab90d7645d5fc7e513cce767597dcf
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-06-18 14:54:00 +0700

    add login, colors, update pubspec

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color primaryColor = Color(0xFFDF001D); // Màu chính
+  static const Color orange = Color(0xFFF9A825); // Màu phụ
+  static const Color textColor = Colors.black; // Màu chữ
+  static const Color hintTextColor = Colors.grey; // Màu gợi ý
+  static const Color buttonTextColor = Colors.white; // Màu chữ trên nút
+  static const Color borderColor = Colors.grey; // Màu viền
+  static const Color shadowColor = Colors.black; // Màu bóng
+}
