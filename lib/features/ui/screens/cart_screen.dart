1fc7199d3953169e4fbfffdd40b7b2af02f946b0
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-07-09 13:53:37 +0700

    edit cart screen

--- lib/features/ui/screens/cart_screen.dart
+++ lib/features/ui/screens/cart_screen.dart
@@ -1,15 +1,51 @@
+import 'package:app_ban_sach/core/constants/style.dart';
+import 'package:app_ban_sach/features/ui/screens/user_screen.dart';
 import 'package:app_ban_sach/features/ui/widgets/appbar.dart';
 import 'package:app_ban_sach/features/ui/widgets/product_pages/cart_product_card.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
-
   @override
   State<CartScreen> createState() => _CartScreenState();
 }
 
 class _CartScreenState extends State<CartScreen> {
+  bool isAllChecked = false;
+  List<CartItem> listCartProCard = [
+    CartItem(
+      imageUrl: "assets/sgk_tv_2_1.jpg",
+      productName: "Sách TV 2 tập 1",
+      price: 25000,
+      oldPrice: 30000,
+    ),
+    CartItem(
+      imageUrl: "assets/sgk_tv_2_1.jpg",
+      productName: "Sách TV 2 tập 2",
+      price: 25000,
+      oldPrice: 30000,
+    ),
+    CartItem(
+      imageUrl: "assets/sgk_tv_2_1.jpg",
+      productName: "Sách TV 2 tập 3",
+      price: 25000,
+      oldPrice: 30000,
+    ),
+  ];
+  void onChangedCheckedBox(bool? value){
+    setState(() {
+      isAllChecked = value ?? true;
+      for (var item in listCartProCard) {
+        item.isChecked = isAllChecked;
+      }
+    });
+  }
+  void onChangedItemChecked(int index, bool? value) {
+    setState(() {
+      listCartProCard[index].isChecked = value ?? false;
+      isAllChecked = listCartProCard.every((e) => e.isChecked);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +53,40 @@ class _CartScreenState extends State<CartScreen> {
       body: Container(
         padding: const EdgeInsets.all(10.0),
         child: SingleChildScrollView(
-          child: CartProductCard(
-            imageUrl: 'assets/sgk_tv_2_1.jpg',
-            productName: 'Giáo trình triết học Mác - Lênin',
-            price: '140.000đ',
-            oldPrice: '175.000đ',
-            onRemove: () {
-              // Xử lý xóa sản phẩm
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Sản phẩm đã được xóa khỏi giỏ hàng')),
-              );
-            },
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children:[ 
+                    Checkbox(
+                      value: isAllChecked,
+                      onChanged: onChangedCheckedBox,
+                      fillColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                        if (states.contains(MaterialState.selected)) {
+                          return MyColors.warningColor; // Màu khi được chọn
+                        }
+                        return MyColors.whiteColor; // Màu khi không được chọn
+                      }),
+                    ),
+                    Text(
+                      "Chọn tất cả ",
+                      style: const TextStyle(
+                        fontSize: MyTextStyle.size_16,
+                        fontWeight: MyTextStyle.semibold,
+                      ),
+                    ),
+                  ]
+              ),
+              Column(
+                children: List.generate(listCartProCard.length, (index) {
+                  return CartProductCard(
+                    item: listCartProCard[index],
+                    onChanged: (value) => onChangedItemChecked(index, value),
+                  );
+                }),
+              ),
+            ],
           ),
         ),
       ),
