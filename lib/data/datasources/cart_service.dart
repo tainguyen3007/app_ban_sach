63fd60d6b6ae280343e084a13b228f943aab4aab
Quốc <144113586+Hquocc@users.noreply.github.com>
2025-07-24 10:25:00 +0700

    chỉnh lại hàm thêm giỏ hàng. thêm 2 lần sp thì chỉ tăng số lượng nhưng không thêm 1 sản phẩm mới vào giỏ hàng

--- lib/data/datasources/cart_service.dart
+++ lib/data/datasources/cart_service.dart
@@ -5,11 +5,28 @@ import 'package:app_ban_sach/data/datasources/db_helper.dart';
 class CartService {
   final String tableName = 'CART';
 
-  // Thêm vào giỏ hàng
-  Future<int> insertCart(Cart cart) async {
-    final db = await DBHelper.instance.database;
-    return await db.insert(tableName, cart.toMap());
+ Future<void> insertCart(Cart cart) async {
+  final db = await DBHelper.instance.database;
+  // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+  final existing = await db.query(
+    tableName,
+    where: 'productId = ?',
+    whereArgs: [cart.productId],
+  );
+  if (existing.isNotEmpty) {
+    final existingCart = Cart.fromMap(existing.first);
+    final newQuantity = existingCart.quantity + cart.quantity;
+    await db.update(
+      tableName,
+      {'quantity': newQuantity},
+      where: 'productId = ?',
+      whereArgs: [cart.productId],
+    );
+  } else {
+    await db.insert(tableName, cart.toMap());
   }
+}
+
 
   // Lấy tất cả sản phẩm trong giỏ hàng của user
   Future<List<Cart>> getCartByUser(int userId) async {
@@ -21,6 +38,8 @@ class CartService {
     );
     return result.map((e) => Cart.fromMap(e)).toList();
   }
+
+
   Future<int> updateCart(int id, int quantity) async {
   final db = await DBHelper.instance.database;
   return await db.update(
