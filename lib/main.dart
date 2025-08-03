da906a5e9e07cdc7a38498c1fbd15fcae6ae8472
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-08-03 10:47:39 +0700

    updata main

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,45 @@
+import 'package:app_ban_sach/config/firebase_options.dart';
+
 import 'package:app_ban_sach/core/constants/style.dart';
-import 'package:app_ban_sach/data/datasources/db_helper.dart';
-import 'package:app_ban_sach/data/datasources/user_service.dart';
-import 'package:app_ban_sach/data/models/user.dart';
 import 'package:app_ban_sach/features/ui/screens/cart_screen.dart';
 import 'package:app_ban_sach/features/ui/screens/home_screen.dart';
 import 'package:app_ban_sach/features/ui/screens/notification_screen.dart';
 import 'package:app_ban_sach/features/ui/screens/user_screen.dart';
 import 'package:app_ban_sach/features/ui/widgets/nav_bottom.dart';
+import 'package:app_ban_sach/firebase_cloud/models/user.dart';
+import 'package:app_ban_sach/firebase_cloud/service/user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Bắt buộc khi dùng async ở main
-  await DBHelper.instance.database;
-  //kiểm tra trạng thái login
-  final prefs = await SharedPreferences.getInstance();
-  final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
-  final user = await UserService().getUserById(await UserService().getCurrentUserId());
-  if(user != null){
-    runApp(MainScreen(isLoggedIn: isLoggedIn, indexPage: 0,user : user));
-  }
-  else {
-    runApp(MainScreen(isLoggedIn: isLoggedIn, indexPage: 0,));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Lấy trạng thái đăng nhập và userId từ SharedPreferences
+  final prefs = await SharedPreferences.getInstance(); 
+  final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  final userId = prefs.getString('userId');
+
+  // Nếu đã đăng nhập và có userId => lấy user từ DB
+  if (isLoggedIn && userId != null) {
+    final user = await UserService.getUserByUid(userId);
+    runApp(MainScreen(
+      isLoggedIn: true,
+      indexPage: 0,
+      user: user,
+    ));
+  } else {
+    // Nếu chưa đăng nhập hoặc thiếu userId => không có user
+    runApp(MainScreen(
+      isLoggedIn: false,
+      indexPage: 0,
+    ));
   }
 }
+
 class MainApp extends StatelessWidget {
   final bool isLoggedIn;
   const MainApp({
@@ -59,7 +75,7 @@ class _MainScreenState extends State<MainScreen> {
   late final List<Widget> _pages = [
     HomeScreen(),
     NotificationScreen(),
-    CartScreen(),
+    CartScreen(userId: 'nguyenviettai770@gmail.com',),
     UserScreen(isLoggedIn: widget.isLoggedIn,user: widget.user),
   ];
 
