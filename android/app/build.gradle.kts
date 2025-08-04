9a630030ea7b552278647507e643079308705574
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-08-04 22:43:26 +0700

    update  enviroment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,7 @@ plugins {
 android {
     namespace = "com.example.app_ban_sach"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
